9d2df006192ef2a137971db01d848acb901097d4
Peter MX <36844547+blakbox23@users.noreply.github.com>
2021-09-14 12:55:25 +0300

    fix linter errors

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit
-          {' '}
+
           <code>src/App.js</code>
           {' '}
           and save to reload.
