30fc04b005209e579d82bd244f01d51d42b40dff
Peter MX <36844547+blakbox23@users.noreply.github.com>
2021-09-16 15:58:27 +0300

    Add calculator functionality

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,46 +1,64 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable react/destructuring-assignment */
+
 import React, { Component } from 'react';
 import '../App.css';
+import calculate from '../logic/calculate';
+import '../logic/operate';
 
 class Calculator extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      total: 0,
+      next: null,
+      operation: null,
+    };
+    this.handleEvent = this.handleEvent.bind(this);
+  }
+
+  handleEvent(e) {
+    const obj = calculate(this.state, e.target.innerText);
+    this.setState(obj);
   }
 
   render() {
     return (
       <div className="calc">
         <div className="screen">
-          0
+          <span>{this.state.total}</span>
+          <span>{this.state.operation}</span>
+          <span>{this.state.next}</span>
         </div>
         <div id="row1">
-          <div id="c1r1">AC</div>
-          <div id="c2r1">+/-</div>
-          <div id="c3r1">%</div>
-          <div id="c4r1" className="operators">/</div>
+          <div id="c1r1" onClick={this.handleEvent}>AC</div>
+          <div id="c2r1" onClick={this.handleEvent}>+/-</div>
+          <div id="c3r1" onClick={this.handleEvent}>%</div>
+          <div id="c4r1" onClick={this.handleEvent} className="operators">÷</div>
         </div>
         <div id="row2">
-          <div id="c1r2">7</div>
-          <div id="c2r2">8</div>
-          <div id="c3r2">9</div>
-          <div id="c4r2" className="operators">*</div>
+          <div id="c1r2" onClick={this.handleEvent}>7</div>
+          <div id="c2r2" onClick={this.handleEvent}>8</div>
+          <div id="c3r2" onClick={this.handleEvent}>9</div>
+          <div id="c4r2" onClick={this.handleEvent} className="operators">x</div>
         </div>
         <div id="row3">
-          <div id="c1r3">4</div>
-          <div id="c2r3">5</div>
-          <div id="c3r3">6</div>
-          <div id="c4r3" className="operators">-</div>
+          <div id="c1r3" onClick={this.handleEvent}>4</div>
+          <div id="c2r3" onClick={this.handleEvent}>5</div>
+          <div id="c3r3" onClick={this.handleEvent}>6</div>
+          <div id="c4r3" onClick={this.handleEvent} className="operators">-</div>
         </div>
         <div id="row4">
-          <div id="c1r4">1</div>
-          <div id="c2r4">2</div>
-          <div id="c3r4">3</div>
-          <div id="c4r4" className="operators">+</div>
+          <div id="c1r4" onClick={this.handleEvent}>1</div>
+          <div id="c2r4" onClick={this.handleEvent}>2</div>
+          <div id="c3r4" onClick={this.handleEvent}>3</div>
+          <div id="c4r4" onClick={this.handleEvent} className="operators">+</div>
         </div>
         <div id="row5">
-          <div id="c1r5">0</div>
-          <div id="c2r5">.</div>
-          <div id="c3r5" className="operators">=</div>
+          <div id="c1r5" onClick={this.handleEvent}>0</div>
+          <div id="c2r5" onClick={this.handleEvent}>.</div>
+          <div id="c3r5" onClick={this.handleEvent} className="operators">=</div>
         </div>
       </div>
     );
