b470be18c6d9d82b068d6bbe609b03ffd73a7cb2
Peter MX <36844547+blakbox23@users.noreply.github.com>
2021-09-22 18:22:00 +0300

    add unit tests

--- src/logic/operate.test.js
+++ src/logic/operate.test.js
@@ -9,9 +9,9 @@ test('subtract 2-1 to equal 1', ()=>{
 test('divide 12÷2 to equal 6', ()=>{
     expect(operate(12, 2, "÷")).toBe("6");
 });
-test('mutiply 12÷0 to return an error message', () => {
-    expect(operate(0,7, "÷")).toBe("Can't divide by 0.")
+test('mutiply 12÷0 to return a 0', () => {
+    expect(operate(0,7, "÷")).toBe("0")
 })
 test('mutplication 12 X 3 to equal 36',() => {
-    expect(operate(12,3,'X')).toEqual(36)
-})
+    expect(operate(12,3,'x')).toBe("36")
+}) 
